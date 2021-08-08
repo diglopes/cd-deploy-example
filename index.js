d12f57a986061349eeb1aab00d2137be9ba543ab
Diego Lopes <diego-chez@hotmail.com>
2021-08-07 23:09:50 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/hello", (_, res) => {
+    console.log("Executing hello route")
+    res.send("Hello world!")
+})
+
+const PORT = process.env.PORT || 1880
+
+app.listen(PORT, () => {
+    console.log("App running on port", PORT)
+})
